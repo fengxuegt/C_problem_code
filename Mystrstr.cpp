e8e616330dda0ce21afcf13fe0344ce59e339a8b
fengxuegt <fengxuegt@163.com>
2021-01-30 21:52:18 +0800

    add Mystrstr

---
+++ Mystrstr.cpp
@@ -0,0 +1,51 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int  kmp(const char * str, const char *target)
+{
+    int count = 0;
+    char* p = (char *) str;
+    char* cur = (char*)target;
+    char* tmp = NULL;
+    while (*p != '\0') {
+        tmp = p;
+        if (*p == *cur) {
+            while (*cur != '\0')
+            {
+                if (*tmp != *cur) {
+                    cur = (char*) target;
+                    break;
+                }
+                cur++;
+                tmp++;
+            }
+            if (*cur == '\0') {
+                return count;
+            }
+
+        }
+        p++;
+        count++;
+    }
+    return -1;
+}
+
+int main()
+{
+    const char *str = "kajflkdnfkwej";
+    const char* target = "kz";
+    int result = kmp(str, target);
+    
+    if (result == -1) {
+        printf("Not found.\n");
+    }
+    else {
+        printf("The index is %d\n", result);
+    }
+    
+    return 0;
+}
+
+
